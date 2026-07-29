70f449f6b9755fc7d9032b8a68dba3a535ccca68
Kidist Meseret <kidistmeseret1@gmail.com>
2026-07-30 01:04:43 +0900

    update style.css and navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <h2>Kidist Meseret</h2>
-      <div className="nav-links">
-        <a href="#about">About</a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+
+      
+      <button 
+        className={`hamburger ${isOpen ? 'active' : ''}`} 
+        onClick={toggleMenu}
+        aria-label="Toggle Navigation"
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+
+      {/* Nav Links */}
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <a href="#about" onClick={() => setIsOpen(false)}>About</a>
+        <a href="#skills" onClick={() => setIsOpen(false)}>Skills</a>
+        <a href="#projects" onClick={() => setIsOpen(false)}>Projects</a>
+        <a href="#contact" onClick={() => setIsOpen(false)}>Contact</a>
       </div>
     </nav>
   );
