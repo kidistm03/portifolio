193475c9e966cadd6487107ad4973a5b239a47c1
Kidist Meseret <kidistmeseret1@gmail.com>
2026-07-29 23:07:33 +0900

    update hero.jsx and add photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,23 @@
+import photo from "../assets/me.jpg";
+
 function Hero({ name, title }) {
   return (
     <section className="hero">
-      <h1>{name}</h1>
-      <h3>{title}</h3>
-      <p>Welcome to my personal portfolio website.</p>
+      <div className="hero-text">
+        <h1>{name}</h1>
+        <h3>{title}</h3>
+
+        <p>
+          Passionate Software Engineering student who loves creating beautiful,
+          responsive websites and learning modern technologies.
+        </p>
+
+        <button>Download CV</button>
+      </div>
+
+      <div className="hero-image">
+        <img src={photo} alt="Kidist Meseret" />
+      </div>
     </section>
   );
 }
